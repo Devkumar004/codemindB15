f81c380fbc4d7a2e6440798a5bd8055497853f0b
Devkumar <jdevkumar000@gmail.com>
2025-05-04 17:30:16 +0530

    today commited

---
+++ src/main/java/GitPr/GitPractice.java
@@ -0,0 +1,15 @@
+package GitPr;
+
+public class GitPractice {
+
+	void task() {
+		System.out.println("Hello");
+		//Hello
+		// This is from Test_New
+		///vbhilgjhjlghlguguhuihuihihhh
+	}
+	public static void main(String[] args) {
+
+	}
+
+}
